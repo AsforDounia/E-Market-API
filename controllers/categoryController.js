3696cd03005b8661b8a06d256d72527eba6b2e54
AsforDounia <asfordounia@gmail.com>
2025-10-07 17:40:56 +0100

    Add functionality to get category by ID

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -10,5 +10,17 @@ async function getAllCategories(req, res){
     }
 }
 
+async function getCategoryById(req, res) {
+    try {
+        const { id } = req.params;
+        const category = await Category.findById(id);
+        if (!category) return res.status(404).json({ message: 'Category not found' });
+        res.status(200).json(category);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: 'Server error', error: err.message });
+    }
+}
 
-export { getAllCategories }
+
+export { getAllCategories, getCategoryById }
--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -6,4 +6,8 @@ import * as categoryController from '../controllers/categoryController.js';
 // Récupérer tous les categories
 categoryRoutes.get('/', categoryController.getAllCategories);
 
+// Récupérer un category par ID
+categoryRoutes.get('/:id', categoryController.getCategoryById);
+
+
 export default categoryRoutes
